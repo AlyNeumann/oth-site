3a173201f77c762d4c9cef5a29f136ba1f5022e4
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-08-26 15:29:00 -0400

    added carousel for images but it is still being jumpy

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './Gallery.css';
+import { Carousel } from 'react-bootstrap';
 
 export default class Gallery extends Component{
     render(){
-        return (<div className="carousel-container">
-        
-            Gallery
-        </div>)
+        return (
+        <Carousel pauseOnHover="true" wrap="true" slide="false">
+            <Carousel.Item className="carousel-image">
+              <img width={640} height={480} alt="900x500" src="Images/carousel1.jpg" />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-image">
+              <img width={640} height={480} alt="900x500" src="Images/carousel2.jpg" />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-image">
+              <img width={640} height={480} alt="900x500" src="Images/carousel3.jpg" />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>)
     }
 }
