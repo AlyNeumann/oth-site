0d88f00643eec7d57ad9c9c4fe02687ca14ef144
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-08-26 15:35:42 -0400

    sizing issue with carousel fixed

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -5,23 +5,23 @@ import { Carousel } from 'react-bootstrap';
 export default class Gallery extends Component{
     render(){
         return (
-        <Carousel pauseOnHover="true" wrap="true" slide="false">
+        <Carousel pauseOnHover="true" wrap="true" className="carousel">
             <Carousel.Item className="carousel-image">
-              <img width={640} height={480} alt="900x500" src="Images/carousel1.jpg" />
+              <img width={900} height={500} alt="900x500" src="Images/carousel1.jpg" />
               <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carousel-image">
-              <img width={640} height={480} alt="900x500" src="Images/carousel2.jpg" />
+              <img width={900} height={500} alt="900x500" src="Images/carousel2.jpg" />
               <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carousel-image">
-              <img width={640} height={480} alt="900x500" src="Images/carousel3.jpg" />
+              <img width={900} height={500} alt="900x500" src="Images/carousel3.jpg" />
               <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
